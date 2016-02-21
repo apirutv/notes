5298f03642c1bd8f150b98bc5cfa332b6cd46bca
Apirut Vanchaam <apirut@live.com>
2016-02-21 17:34:23 +0000

    commit

---
+++ install-notes.sh
@@ -0,0 +1,22 @@
+
+# install nmon
+sudo apt-get install nmon
+
+# install and config git
+sudo apt-get install git
+git config --global user.name "Apirut Vanchaam"
+git config --global user.email "apirut@live.com"
+git config --list # show all configs
+
+# git command
+git clone git@github.com:apirutv/hello-world
+git remote -v # show the origin url
+git remote show [origin]
+git fetch [remote name]
+git push
+git status 
+git status -s
+
+sudo apt-get install open-jdk...
+
+sudo apt-get install maven
